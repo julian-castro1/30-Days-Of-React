4e98fc859fdfa6d7c4571bf25f013e4ec067d4bd
Julian Castro <julian_castro1@icloud.com>
2023-09-05 23:05:28 -0500

    day-04

--- solutions/day-03/src/UserCard.js
+++ solutions/day-03/src/UserCard.js
@@ -1,12 +1,16 @@
 import ProfilePic from "./ProfilePic.jsx";
 import verifiedIcon from "./images/png-clipart-computer-icons-check-mark-symbol-symbol-miscellaneous-blue.png"
 import time from "./images/time.png"
+import { hexaColor } from "./hexGen.js";
 
 
 function UserCard({name, verified, role, location, skills, profilePic, joinedDate}) {
 
     let roleText = `${role}, ${location}`
     let joinedText = `Joined on ${joinedDate}`
+    let skillStyle = {
+      backgroundColor: hexaColor(),
+    };
 
   return (
     <div className="usercard">
@@ -19,7 +23,8 @@ function UserCard({name, verified, role, location, skills, profilePic, joinedDat
         <h1 className="skills-title">SKILLS</h1>
         <div className="skills">
             {skills.map((skill) => {
-                return <div className="skill_container">{skill}</div>
+                let color = { backgroundColor: hexaColor() };
+                return <div id={skill} style={color} className="skill_container">{skill}</div>
             })}
         </div>
         <div className="joined">
