dbb35d9acaac7dfec6945d1137bc3fc74ac7ce22
Julian Castro <julian_castro1@icloud.com>
2023-09-05 23:37:15 -0500

    day-06:colors

--- solutions/day-03/src/App.js
+++ solutions/day-03/src/App.js
@@ -2,22 +2,37 @@ import './App.css';
 import UserCard from "./UserCard";
 import profilePic from "./images/Julian_Gregory_Castro_16Aug2021_Austin-South.jpeg"
 import "./UserCard.css";
+import "./colors.css";
+import { hexaColor } from './hexGen';
 
 function App() {
 
   let skills = ["React", "HTML", "JS", "CSS", "Node", "Python"]
+  let colors = [];
+  for(let i = 0; i < 32; i++){
+    colors.push(hexaColor());
+  }
 
   return (
-    <UserCard
-      // {name, verified, role, location, skills, profilePic, joinedDate}
-      name = "Julian Castro"
-      verified = {true}
-      role = "Software Engineer"
-      location = "Houston, TX"
-      skills = {skills}
-      profilePic = {profilePic}
-      joinedDate = "August 16, 2021"
-    />
+    <>
+      <div className='color_title'>Colors</div>
+      <div className='colorGrid'>
+        {colors.map((color) => {
+          let style = {backgroundColor: color};
+          return <div className='color' id={color} style={style}>{color}</div>
+        })}
+      </div>
+    </>
+    // <UserCard
+    //   // {name, verified, role, location, skills, profilePic, joinedDate}
+    //   name = "Julian Castro"
+    //   verified = {true}
+    //   role = "Software Engineer"
+    //   location = "Houston, TX"
+    //   skills = {skills}
+    //   profilePic = {profilePic}
+    //   joinedDate = "August 16, 2021"
+    // />
   );
 }
 
