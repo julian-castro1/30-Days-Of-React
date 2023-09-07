1e86732677e5f2ce78fbfeccab3b59db25f8601a
Julian Castro <julian_castro1@icloud.com>
2023-09-07 11:16:11 -0500

    day09

--- solutions/day-04/src/App.jsx
+++ solutions/day-04/src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { countriesData } from './countries'
 import CountryCard from './CountryCard'
 import './App.css'
 
 function App() {
   const [index, changeCountry] = useState(0)
+  const [loading, setLoading] = useState(false)
 
-  const handleClick = () => { changeCountry(index+1)}
+  const handleClick = () => {
+    setLoading(true)
+    changeCountry(index+1)
+  }
+ 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    // Cleanup timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, [index]); 
+
 
   return (
     <>
-      <CountryCard
-          country={countriesData[index]}
-          onClick={handleClick}
-      />
+      {loading ? 
+        <p>loading...</p> :
+        <CountryCard
+                country={countriesData[index]}
+                onClick={handleClick}
+              />
+      }
     </>
   )
 }
